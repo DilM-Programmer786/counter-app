907c84c5384922db95f75ff72eeef4527fe08c8e
Dil Muhammad <63668567+DilM-Programmer786@users.noreply.github.com>
2022-03-16 10:46:20 -0400

    Complete Commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {Link} from 'react-router-dom'
 
 const About = () => {
   return (
@@ -6,7 +6,7 @@ const About = () => {
      <h4>
          Version 1.0.0 
      </h4>
-     <a href="./"> Go Back </a>
+     <Link to="/"> Go Back </Link>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 // import  PropTypes from 'prop-types';
 import Button from './Button';
+import { useLocation } from 'react-router-dom';
 
 const Header = ({title, onAdd, showAdd}) => {
+
+  const location = useLocation()
 
   //  const onClick = () => {
   // console.log("Header Prop Clicked");
@@ -13,7 +16,12 @@ const Header = ({title, onAdd, showAdd}) => {
        <h1>
         {title}
        </h1>
-       <Button color={showAdd ? 'red' : 'green' } text={showAdd ? 'Close' : 'Add' } onClick={onAdd}/>
+       {location.pathname ==='/' &&
+       (<Button 
+         color={showAdd ? 'red' : 'green' } 
+         text={showAdd ? 'Close' : 'Add' } 
+         onClick={onAdd}/>
+       )}
     </div>
   )
 }
